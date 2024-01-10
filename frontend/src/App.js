bb0dbc5e4e79c1088d5e2dd92846fc4e29904597
sujeet <vishsujeet2806@gmail.com>
2024-01-10 23:02:50 +0530

    creating app, header, footer,

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
 
+import React from "react";
 import './App.css';
-import Header from "./component/layout/Header.js"
+import Header from './component/layout/Header/Header.js';
+import Footer from './component/layout/Footer/Footer.js';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import webfont from "webfontloader";
+// import Home from "./component/Home/Home.js";
+
+
 
 function App() {
+
+  React.useEffect(() => {
+    webfont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
+
   return (
+    <Router>
+      <Header />
+      {/* <Route exact path = "/" component= {home}>
 
+      </Route> */}
 
-    <Header />
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+     <p>Welcome to Ecommerce</p>
+     <h1>FIND AMAZING PRODUCTS BELOW</h1>
+    </>
+  )
+}
+
+export default Home
